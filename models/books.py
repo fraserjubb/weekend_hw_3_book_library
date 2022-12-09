05c65bc44fa31e04c3f9ab4a21e69e9871fc7d52
Fraser Jubb <fraser.jubb@gmail.com>
2022-12-09 14:38:42 +0000

    Set up all files and tested that local host is working correctly

---
+++ models/books.py
@@ -0,0 +1,8 @@
+from models.book import *
+
+book_1 = Book("Mistborn: The Final Empire", "Brandon Sanderson", "High Fantasy")
+book_2 = Book("Storm Front", "Jim Butcher", "Urban Fantasy")
+book_3 = Book("Dune", "Frank Herbert", "Science Fiction")
+
+
+books = [book_1, book_2, book_3]
