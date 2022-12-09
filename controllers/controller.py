9c4f37de5bd65ebb32e28226eea736f797490a7c
Fraser Jubb <fraser.jubb@gmail.com>
2022-12-09 16:42:43 +0000

    All books can be seen in list, books can be added and removed.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,26 @@
 from flask import render_template, request, redirect
 from app import app
-from models.books import books
+from models.books import books, add_new_book_to_library, remove_book_from_library
 from models.book import *
-# import datetime
 
 @app.route('/books')
 def index():
     return render_template('index.html', title='Home', books=books)
+
+@app.route('/books', methods=['POST'])
+def add_new_book():
+    title = request.form['title']
+    author = request.form['author']
+    genre = request.form['genre']
+    new_book = Book(title=title, author=author, genre=genre)
+    add_new_book_to_library(new_book)
+    return redirect('/books')
+
+@app.route('/books/delete/<index>', methods=['POST'])
+def delete_book(index):
+    remove_book_from_library(int(index))
+    return redirect('/books')
+
+
+
+
--- models/books.py
+++ models/books.py
@@ -6,3 +6,9 @@ book_3 = Book("Dune", "Frank Herbert", "Science Fiction")
 
 
 books = [book_1, book_2, book_3]
+
+def add_new_book_to_library(book):
+    books.append(book)
+
+def remove_book_from_library(index):
+    books.pop(index)
